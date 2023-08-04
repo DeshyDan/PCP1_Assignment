98361301a346ab08330303642c8e78afa96aa779
Deshy Dan <deshy733@gmail.com>
2023-08-04 14:06:01 +0200

    Added Grid searcher starter code

---
+++ src/GridSearcher.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.RecursiveAction;
+
+public class GridSearcher extends RecursiveAction{
+
+    @Override
+    protected void compute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compute'");
+    }
+    
+}
