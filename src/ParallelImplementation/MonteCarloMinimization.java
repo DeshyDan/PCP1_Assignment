2d0a810eb206dacfecdececbc3ae1eb0ec510ad8
Deshy Dan <deshy733@gmail.com>
2023-08-08 17:11:53 +0200

    Fixed memory leakage with fork join pool

--- src/ParallelImplementation/MonteCarloMinimization.java
+++ src/ParallelImplementation/MonteCarloMinimization.java
@@ -35,29 +35,30 @@ public class MonteCarloMinimization {
         int finder = -1;
 
         GridSearcher gridSearch = new GridSearcher(searches, numSearches );
-        ForkJoinPool pool = new ForkJoinPool();
-        var result= pool.invoke(gridSearch);
-        tock();
+        try (ForkJoinPool pool = new ForkJoinPool()) {
+            var result= pool.invoke(gridSearch);
+            tock();
 
-        numSearches = result.numSearches;
-        int min = result.localMin;
-        finder = result.finder;
+            numSearches = result.numSearches;
+            int min = result.localMin;
+            finder = result.finder;
 
-        System.out.printf("Run parameters\n");
-        System.out.printf("\t Rows: %d, Columns: %d\n", rows, columns);
-        System.out.printf("\t x: [%f, %f], y: [%f, %f]\n", xmin, xmax, ymin, ymax);
-        System.out.printf("\t Search density: %f (%d searches)\n", searches_density, numSearches);
+            System.out.printf("Run parameters\n");
+            System.out.printf("\t Rows: %d, Columns: %d\n", rows, columns);
+            System.out.printf("\t x: [%f, %f], y: [%f, %f]\n", xmin, xmax, ymin, ymax);
+            System.out.printf("\t Search density: %f (%d searches)\n", searches_density, numSearches);
 
-        /* Total computation time */
-        System.out.printf("Time: %d ms\n", endTime - startTime);
-        int tmp = terrain.getGrid_points_visited();
-        System.out.printf("Grid points visited: %d  (%2.0f%s)\n", tmp, (tmp / (rows * columns * 1.0)) * 100.0, "%");
-        tmp = terrain.getGrid_points_evaluated();
-        System.out.printf("Grid points evaluated: %d  (%2.0f%s)\n", tmp, (tmp / (rows * columns * 1.0)) * 100.0, "%");
+            /* Total computation time */
+            System.out.printf("Time: %d ms\n", endTime - startTime);
+            int tmp = terrain.getGrid_points_visited();
+            System.out.printf("Grid points visited: %d  (%2.0f%s)\n", tmp, (tmp / (rows * columns * 1.0)) * 100.0, "%");
+            tmp = terrain.getGrid_points_evaluated();
+            System.out.printf("Grid points evaluated: %d  (%2.0f%s)\n", tmp, (tmp / (rows * columns * 1.0)) * 100.0, "%");
 
-        /* Results */
-        System.out.printf("Global minimum: %d at x=%.1f y=%.1f\n\n", min,
-                terrain.getXcoord(searches[finder].getPos_row()), terrain.getYcoord(searches[finder].getPos_col()));
+            /* Results */
+            System.out.printf("Global minimum: %d at x=%.1f y=%.1f\n\n", min,
+                    terrain.getXcoord(searches[finder].getPos_row()), terrain.getYcoord(searches[finder].getPos_col()));
+        }
 
     }
 
